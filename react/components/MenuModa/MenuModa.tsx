9fafa1010106d01a55b42d08a178acda0ec8d40b
Daniel Ramirez <dramirezh@estilos.com.pe>
2024-06-10 10:05:28 -0500

    feat: Componente menu moda

---
+++ react/components/MenuModa/MenuModa.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+function MenuModa() {
+    return (  <div>
+
+        
+
+    </div>);
+}
+
+export default MenuModa;
--- react/components/TodaysOffers/TodaysOffers.tsx
+++ react/components/TodaysOffers/TodaysOffers.tsx
@@ -1,14 +1,68 @@
 import React from "react";
+import styles from "./styles.css";
+import ProductThumb from "./ProductThumb";
+import Slider from "../Slider/Slider";
+
 function TodaysOffers() {
-    return ( 
-        <div>
-            <div>
-                <h3>Ofertas de hoy</h3>
-                <p>Las promociones se agotan rápidamente, asegúrate para no perderlas</p>
-                
-            </div>
-        </div> 
-    );
+  return (
+    <div className={styles.todaysOffersCont}>
+      <div className={styles.productInfo}>
+        <div className={styles.sectionTitle}>Today offers</div>
+        <div className={styles.sectionDescription}>
+          <p>
+            Promotions are selling out quickly, hurry up so you don't miss out
+          </p>
+        </div>
+        <div className={`${styles.bzotechCountdown} flex-wrapper`} data-date="04/25/2025">
+          <div className={`${styles.clock} ${styles.day}`}>
+            <strong className="number title34 font-semibold">329</strong>
+            <span className="text title20 font-medium">DAYS</span>
+          </div>
+          <div className={`${styles.clock} ${styles.hour}`}>
+            <strong className="number title34 font-semibold">15</strong>
+            <span className="text title20 font-medium">HOURS</span>
+          </div>
+          <div className={`${styles.clock} ${styles.min}`}>
+            <strong className="number title34 font-semibold">31</strong>
+            <span className="text title20 font-medium">MINS</span>
+          </div>
+          <div className={`${styles.clock} ${styles.sec}`}>
+            <strong className="number title34 font-semibold">29</strong>
+            <span className="text title20 font-medium">SECS</span>
+          </div>
+        </div>
+        <div className={styles.actionButtons}>
+          <button
+            // type="hidden"
+            name="addtocart-ajax-nonce"
+            className={styles.buttonBuy}
+
+          >
+          <span className={styles.text}>Add to cart</span>
+          </button>
+        </div>
+      </div>
+      
+      <Slider
+              // infinite={true}
+              // showNavigationArrows="never"
+            //   showPaginationDots={
+            //    " "orientation === "vertical" ? "always" : "never"""
+            //   }
+              // usePagination={true}
+              // fullWidth={false}
+              // arrowSize={5}
+              // itemsPerPage={1}
+              // centerMode="disabled"
+            //   centerModeSlidesGap            
+            >
+            <ProductThumb/>
+            <ProductThumb/>
+            <ProductThumb/>
+    </Slider> 
+      
+    </div>
+  );
 }
 
 export default TodaysOffers;
