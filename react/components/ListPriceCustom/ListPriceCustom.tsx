3297b85ad73131a4e172bb3358d54290f9435919
Daniel Ramirez <dramirezh@estilos.com.pe>
2024-07-10 12:09:21 -0500

    feat: quitar log en listPrice

--- react/components/ListPriceCustom/ListPriceCustom.tsx
+++ react/components/ListPriceCustom/ListPriceCustom.tsx
@@ -1,84 +1,77 @@
-import React from 'react';
-import styles from './styles.css';
-import { useProductInfo } from '../../hooks/productInfoHook'; 
-import { useListPrice } from '../../hooks/listPriceHook'; 
+import React from 'react'
+import styles from './styles.css'
+import { useProductInfo } from '../../hooks/productInfoHook'
+import { useListPrice } from '../../hooks/listPriceHook'
 
-function ListPrice({ container , isProduct}:any) {
-  const { selectedItem, product } = useProductInfo();
-  const productId =product?.productId
-  console.log('productId ',productId);
-  
-  const { price,isVoid } = useListPrice(selectedItem, productId, container);
+function ListPrice({ container, isProduct }: any) {
+  const { selectedItem, product } = useProductInfo()
+  const productId = product?.productId
+  // console.log('productId ',productId);
+
+  const { price, isVoid } = useListPrice(selectedItem, productId, container)
 
   if (!price || isVoid) {
-    return null; 
+    return null
   }
 
-  const formattedPrice = price.toLocaleString('es-PE', { style: 'currency', currency: 'PEN' });
+  const formattedPrice = price.toLocaleString('es-PE', {
+    style: 'currency',
+    currency: 'PEN',
+  })
 
   return (
     <div className={styles.listPrice}>
       {isProduct && <del className={styles.normalText}>Normal</del>}
       <del>{formattedPrice}</del>
     </div>
-  );
+  )
 }
 
-
-
 ListPrice.defaultProps = {
-    isProduct:false,
-    container: [
-        {
-            productId:191,
-            fakePrice:19100
-        },
-        {
-            productId:190,
-            fakePrice:19000
-        },
+  isProduct: false,
+  container: [
+    {
+      productId: 191,
+      fakePrice: 19100,
+    },
+    {
+      productId: 190,
+      fakePrice: 19000,
+    },
+  ],
+}
 
-]
-    
-  };
-  
 ListPrice.schema = {
-    title: 'Agregue precios blancos artificiales',
-    description:
-      'Puedes alterar el orden de este componente',
-    type: 'object',
-    properties: {
-      isProduct: {
-        type :'boolean',
-        title: '¿Es PDP?',
-        default: true,
-      },
-      container: {
-        title: 'Contenedor de validación rutas y contenido SEO',
-        type: 'array',
-        items: {
-          title: 'Item ',
-          type: 'object',
-          properties: {
-            productId: {
-              title: 'productId',
-              description: 'productId',
-              type: 'string',
-            },
-            fakePrice: {
-              title: 'fakePrice',
-              description: 'fakePrice',
-              type: 'string',
-            },
+  title: 'Agregue precios blancos artificiales',
+  description: 'Puedes alterar el orden de este componente',
+  type: 'object',
+  properties: {
+    isProduct: {
+      type: 'boolean',
+      title: '¿Es PDP?',
+      default: true,
+    },
+    container: {
+      title: 'Contenedor de validación rutas y contenido SEO',
+      type: 'array',
+      items: {
+        title: 'Item ',
+        type: 'object',
+        properties: {
+          productId: {
+            title: 'productId',
+            description: 'productId',
+            type: 'string',
+          },
+          fakePrice: {
+            title: 'fakePrice',
+            description: 'fakePrice',
+            type: 'string',
           },
         },
       },
     },
-  };
-  
-  export default ListPrice
-  
+  },
+}
 
-
-
-
+export default ListPrice
