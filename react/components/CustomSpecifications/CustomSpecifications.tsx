23744ef6c0fc9525a1da744837958862653450ce
Daniel Ramirez <dramirezh@estilos.com.pe>
2024-07-10 10:38:14 -0500

    fix: corregir especificaciones btn mostrar menos

--- react/components/CustomSpecifications/CustomSpecifications.tsx
+++ react/components/CustomSpecifications/CustomSpecifications.tsx
@@ -1,58 +1,71 @@
-import React, { useState, useEffect } from "react";
-import { useProduct } from "vtex.product-context";
-import styles from "./styles.css";
+import React, { useState, useEffect } from 'react'
+import { useProduct } from 'vtex.product-context'
+import useResize from '../../hooks/sizeScreenHook'
+import styles from './styles.css'
 
 function CustomDescription() {
-    const [showMore, setShowMore] = useState(false);
-    const [sectionHeight, setSectionHeight] = useState(0);
-    const contextPdp = useProduct();
-    const { product } = contextPdp;
-    const [content, setContent] = useState('')
+  const [showMore, setShowMore] = useState(false)
+  const [sectionHeight, setSectionHeight] = useState(0)
+  const contextPdp = useProduct()
+  const { product } = contextPdp
+  const [content, setContent] = useState('')
+  const [, isMobil] = useResize()
 
-    useEffect(() => {
-        if (product?.description) {
-            const arrToSplit = product?.description?.split("&&");
-            let cont = arrToSplit ? arrToSplit[0] : "";
-            setContent(cont);
-        }
-    }, [product]);
+  useEffect(() => {
+    if (product?.description) {
+      const arrToSplit = product?.description?.split('&&')
+      let cont = arrToSplit ? arrToSplit[0] : ''
+      setContent(cont)
+    }
+  }, [product])
 
-    useEffect(() => {
-        const section = document.getElementById("custom-description-section");
-        if (section) {
-            setSectionHeight(section.offsetHeight);
-        }
-    }, [product?.description]);
+  useEffect(() => {
+    const section = document.getElementById('custom-description-section')
+    if (section) {
+      setSectionHeight(section.offsetHeight)
+    }
+  }, [product?.description])
 
-    const toggleShowMore = () => {
-        setShowMore(!showMore);
-    };
+  const toggleShowMore = () => {
+    setShowMore(!showMore)
+  }
 
-    return (
-        <div style={{ position: "relative" }} className={styles.customSpecificationsContainer}>
-            <section id="custom-description-section"
-                className={styles.contentSpecifications}
-                style={{ maxHeight: `${!showMore ? "100%" : "80px"}` }}
+  return (
+    <div
+      style={{ position: 'relative' }}
+      className={styles.customSpecificationsContainer}
+    >
+      <section
+        id="custom-description-section"
+        className={styles.contentSpecifications}
+        style={{ maxHeight: `${!showMore ? '100%' : '280px'}` }}
+      >
+        <div dangerouslySetInnerHTML={{ __html: content }} />
+      </section>
+      <div className="vtex-store-components-3-x-pointerEventsNone  bottom-0 w-100 h-100 flex-column justify-end flex">
+        <div
+          className={
+            showMore ? 'vtex-store-components-3-x-fadeBottom w-100 h-50' : ''
+          }
+          style={{ transition: 'all 400ms ease-in-out 0s' }}
+        ></div>
+        {isMobil && sectionHeight > 280 && (
+          <div
+            className={`${styles.containerShowMore} vtex-store-components-3-x-pointerEventsAuto tc w-100 bg-base`}
+          >
+            <button
+              onClick={() => {
+                toggleShowMore()
+              }}
+              className="vtex-store-components-3-x-showMoreButton c-action-primary t-action pointer ma5 bn outline-0"
             >
-                <div
-                    dangerouslySetInnerHTML={{ __html: content }}
-                />
-            </section>
-            <div className="vtex-store-components-3-x-pointerEventsNone  bottom-0 w-100 h-100 flex-column justify-end flex">
-                <div
-                    className={showMore ? "vtex-store-components-3-x-fadeBottom w-100 h-50" : ""}
-                    style={{ transition: "all 400ms ease-in-out 0s" }}
-                ></div>
-                {sectionHeight > 80 && (
-                    <div className={`${styles.containerShowMore} vtex-store-components-3-x-pointerEventsAuto tc w-100 bg-base`}>
-                        <button onClick={() => { toggleShowMore() }} className="vtex-store-components-3-x-showMoreButton c-action-primary t-action pointer ma5 bn outline-0">
-                            {showMore ? "Mostrar más" : "Mostrar menos"}
-                        </button>
-                    </div>
-                )}
-            </div>
-        </div>
-    );
+              {showMore ? 'Mostrar más' : 'Mostrar menos'}
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+  )
 }
 
-export default CustomDescription;
+export default CustomDescription
