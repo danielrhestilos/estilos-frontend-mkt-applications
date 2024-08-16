046e222969c10be820492d859768bb5b9b692ec1
Daniel Ramirez <dramirezh@estilos.com.pe>
2024-08-16 11:34:11 -0500

    feat

--- react/components/EstilosPrice/components/RenderPromotionInPDP.tsx
+++ react/components/EstilosPrice/components/RenderPromotionInPDP.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react'
 import { useCssHandles } from 'vtex.css-handles'
 import { FormattedCurrency } from 'vtex.format-currency'
 import './styles/styles.css'
@@ -6,7 +6,7 @@ import './styles/styles.css'
 const CSS_HANDLES = [
   'promotionEstilos_containerIcon',
   'promotionEstilos_titleName',
-  'promotionEstilos_titleProduct'
+  'promotionEstilos_titleProduct',
 ]
 
 const PromotionMessage = ({ installments }: any) => {
@@ -18,23 +18,25 @@ const PromotionMessage = ({ installments }: any) => {
 
   return (
     <div className="lh-solid">
-      <span className="f7">{defaultMessage}</span>
+      <span className="f7" style={{ color: '#5A5A5A' }}>
+        {defaultMessage}
+      </span>
     </div>
   )
 }
-                                          
+
 const PromotionDetails = ({ priceProduct, installments }: any) => {
   const handles = useCssHandles(CSS_HANDLES)
   return (
     <div className="flex w-100 justify-between">
-      <div className="w-100 flex">
+      <div className={`${handles.promotionEstilos_containerIcon} w-40`}></div>
+      <div className="w-100 flex" style={{ marginLeft: '5px' }}>
         <div className="w-100">
           <span className={`${handles.promotionEstilos_titleName} f4`}>
             Tarjeta estilos
           </span>
           <PromotionMessage installments={installments} />
         </div>
-        <div className={`${handles.promotionEstilos_containerIcon} w-40`}></div>
       </div>
       <div className="w-40 flex justify-end items-center f4">
         <span className={`${handles.promotionEstilos_titleProduct}`}>
