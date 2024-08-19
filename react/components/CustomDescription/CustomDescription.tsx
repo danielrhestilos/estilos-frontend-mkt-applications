d2296fdb072d005c54d96cfb762960ab61fe7603
Daniel Ramirez <dramirezh@estilos.com.pe>
2024-08-19 15:32:33 -0500

    nuevo campo para descripciones

--- react/components/CustomDescription/CustomDescription.tsx
+++ react/components/CustomDescription/CustomDescription.tsx
@@ -1,30 +1,45 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from 'react'
 import { useProduct } from 'vtex.product-context'
 
 function CustomDescription() {
+  const contextPdp = useProduct()
+  const { product } = contextPdp
+  const [content, setContent] = useState<any>(null)
+  const hasFetched = useRef(false) // Para rastrear si ya se realizó el fetch
 
-    const contextPdp = useProduct()
-    const { product } = contextPdp
-    const [content, setContent] = useState('')
+  useEffect(() => {
+    if (!product || hasFetched.current) return
 
-    useEffect(() => {
-        if (product?.description) {
-            const arrToSplit = product?.description?.split("&&");
-            let cont = arrToSplit ? arrToSplit[1] : "";
-            setContent(cont);
-        }
-    }, [product]);
+    const { productId } = product
+    hasFetched.current = true // Marcar como true para que no vuelva a hacer fetch
 
+    fetch('/_v/validate-product/' + productId)
+      .then((resp: any) => resp.json())
+      .then((data: any) => {
+        console.log('data', data)
+        // if (data?.targetProduct?.DescriptionShort == '') {
+        //   setContent('')
+        // }
+        setContent(data?.targetProduct?.DescriptionShort)
+      })
+  }, [product])
+
+  if (content == null) {
     return (
-        <>
-            <div>
-                <p
-
-                    dangerouslySetInnerHTML={{ __html: content }}
-                />
-            </div>
-        </>
+      <div style={{ margin: 'auto', textAlign: 'center' }}>
+        <img
+          style={{ height: '40px' }}
+          src="https://estilospe.vtexassets.com/assets/vtex.file-manager-graphql/images/5e0d4997-cc7b-4c36-9ed6-6164dca8905c___b4f4d1d401c8cfd7d3fbbc01d83465fc.gif"
+        />
+      </div>
     )
+  }
+
+  return (
+    <div>
+      <p dangerouslySetInnerHTML={{ __html: content }} />
+    </div>
+  )
 }
 
-export default CustomDescription;
+export default CustomDescription
