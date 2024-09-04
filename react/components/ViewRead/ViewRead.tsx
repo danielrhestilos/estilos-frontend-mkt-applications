ef036cec2b7836483cf2992dda0bec3724ed7aab
Daniel Ramirez <dramirezh@estilos.com.pe>
2024-09-04 10:49:46 -0500

    feat: slider de productos visitados

--- react/components/ViewRead/ViewRead.tsx
+++ react/components/ViewRead/ViewRead.tsx
@@ -5,15 +5,41 @@ import useLocalStorage from '../../hooks/localStorageHook'
 function ViewRead() {
   const { product } = useProduct()
   const [productsV, setProductsV] = useLocalStorage('productsV', [])
+  // Create our number formatter.
+  const formatter = new Intl.NumberFormat('es-PE', {
+    style: 'currency',
+    currency: 'PEN',
+
+    // These options are needed to round to whole numbers if that's what you want.
+    //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
+    //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
+  })
 
   useEffect(() => {
     // Comprobación si el producto ya existe en el arreglo
     const productExists = productsV.some(
       (p: any) => p.productId === product.productId
     )
+    // console.log('product.priceRange.listPrice.lowPrice ', product.priceRange)
+    // console.log('product.priceRange.sellingPrice.lowPrice ', product.priceRange)
 
-    if (!productExists) {
-      const newProducts = [product, ...productsV]
+    if (
+      !productExists &&
+      product.priceRange.listPrice.highPrice != 0 &&
+      product.priceRange.sellingPrice.highPrice != 0
+    ) {
+      const newProducts = [
+        {
+          ...product,
+          listPriceFormated: formatter.format(
+            product.priceRange.listPrice.lowPrice
+          ),
+          bestPriceFormated: formatter.format(
+            product.priceRange.sellingPrice.lowPrice
+          ),
+        },
+        ...productsV,
+      ]
 
       // Limitar a un máximo de 20 elementos
       if (newProducts.length > 20) {
@@ -22,7 +48,8 @@ function ViewRead() {
 
       setProductsV(newProducts)
     }
-  }, [product, productsV, setProductsV])
+  }, [product /*,
+    -productsV, setProductsV*/])
 
   return <></>
 }
