da7a5eabbc36dd9ba1a9640cf3b89a06b9db2cfc
Daniel Ramirez <dramirezh@estilos.com.pe>
2025-01-09 10:38:51 -0500

    feat: cmabiar msj Yuri sug

--- react/components/IfAvailableWL/IfAvailableWL.tsx
+++ react/components/IfAvailableWL/IfAvailableWL.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import { useSimulation } from '../../hooks/simulationHook'
 import useLocalStorage from '../../hooks/localStorageHook'
 import useProduct from 'vtex.product-context/useProduct'
-import styles from './styles.module.css'
+// import styles from './styles.module.css'
 
 function IfAvailableWL({
   children,
@@ -36,18 +36,13 @@ function IfAvailableWL({
                 <>
                   {' '}
                   <span style={{ fontSize: '13px', fontWeight: '600' }}>
-                    {'⚠️ No disponible en '} {localDistrito}
-                  </span>
-                  <a
-                    className={styles.moreProducts}
-                    href={
-                      productContext?.product?.categoryTree?.[
-                        productContext?.product?.categoryTree?.length - 1
-                      ]?.href
+                    {'⚠️ No disponible en '} {localDistrito},{' '}
+                    {
+                      'Puedes optar por delivery o recogerlo en una tienda de otra ciudad.'
                     }
-                  >
-                    Ver más productos
-                  </a>
+                  </span>
+                  <br />
+                  <>{children}</>
                 </>
               ) : (
                 <></>
