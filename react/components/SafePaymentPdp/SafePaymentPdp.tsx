fcaa1a38e47812e274019aac8733d5edbccec89a
Daniel Ramirez <dramirezh@estilos.com.pe>
2025-04-30 08:38:32 -0500

    feat: cambiar mesjs y srcs

--- react/components/SafePaymentPdp/SafePaymentPdp.tsx
+++ react/components/SafePaymentPdp/SafePaymentPdp.tsx
@@ -6,8 +6,8 @@ interface SafePaymentPdpProps {}
 const SafePaymentPdp: React.FC<SafePaymentPdpProps> = () => {
   return (
     <div className={styles.container}>
-      <span>Transacciones protegidas:</span>
-      <img src="https://estilospe.vtexassets.com/arquivos/bann-pasarella-tarjetas-seguras-debito-290425.svg" />
+      <span>Compra segura garantizada: </span>
+      <img src="https://estilospe.vtexassets.com/arquivos/bann-pasarella-tarjetas-seguras-290425.svg" />
     </div>
   )
 }
