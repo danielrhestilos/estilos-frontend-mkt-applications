150b6db09694c68c53604ad38e7ee0e679008bd0
Daniel Ramirez <dramirezh@estilos.com.pe>
2025-08-08 16:30:03 -0500

    feat: corregir importaciones

---
+++ react/components/Video/Video.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+interface VideoPlayerProps {
+    url: string
+}
+
+const VideoPlayer: React.FC<VideoPlayerProps> = ({ url }) => {
+    const isYouTubeOrVimeo =
+        url.includes('youtube.com') || url.includes('vimeo.com')
+
+    if (isYouTubeOrVimeo) {
+        return (
+            <div>
+                <iframe
+                    src={url}
+                    title="Video"
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                    className="w-full h-full"
+                />
+            </div>
+        )
+    }
+
+    return (
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <video
+                autoPlay
+                // loop
+                muted
+                className="w-full rounded-xl shadow-md"
+                style={{ margin: 'auto', height: 'auto', width: '100%' }}
+            >
+                <source src={url} type="video/mp4" />
+                Tu navegador no soporta la reproducción de video.
+            </video>
+        </div>
+    )
+}
+
+export default VideoPlayer
